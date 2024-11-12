6184056264f676624ea8ed369fba94779d118b01
Jker <143986198+Jumpker@users.noreply.github.com>
2024-11-12 16:50:21 +0800

    栈的使用 - 回文

--- 7. 数据结构/作业/第八周/p82算法2 (2).cpp
+++ 7. 数据结构/作业/第八周/p82算法2 (2).cpp
@@ -1,26 +1,26 @@
-//(2)������ָ��������������ͬ���ַ����У��硰abba���͡�abdba��
-//���ǻ��ģ�����good�����ǻ��ġ�������㷨�ж��������ַ�����
-//�Ƿ�Ϊ���ġ�����ʾ����һ���ַ���ջ����
+//(2)回文是指正读、反读均相同的字符序列，如“abba”和“abdba”
+//均是回文，但“good”不是回文。试设计算法判定给定的字符序列
+//是否为回文。（提示：将一半字符入栈。）
 #include <stdio.h>  
 #include "sqstack.cpp" 
-// �ж��ַ����Ƿ�Ϊ����  
+// 判断字符串是否为回文  
 bool isPalindrome(const char *str) {  
     Stack s;  
     initStack(&s);  
     int len = strlen(str);  
     int mid = len / 2;  
   
-    // ��ǰ�벿���ַ�ѹ��ջ��  
+    // 将前半部分字符压入栈中  
     for (int i = 0; i < mid; i++) {  
         push(&s, str[i]);  
     }  
   
-    // ����ַ�������Ϊ�������������м��ַ�  
+    // 如果字符串长度为奇数，则跳过中间字符  
     if (len % 2 != 0) {  
         mid++;  
     }  
   
-    // �Ӻ�벿�ֿ�ʼ�Ƚ��ַ�  
+    // 从后半部分开始比较字符  
     for (int i = mid; i < len; i++) {  
         if (pop(&s) != str[i]) {  
             return false;  
@@ -32,13 +32,13 @@ bool isPalindrome(const char *str) {
   
 int main() {  
     char str[MAX_SIZE];  
-    printf("������һ���ַ�����");  
+    printf("请输入一个字符串：");  
     scanf("%s", str);  
   
     if (isPalindrome(str)) {  
-        printf("�ַ����ǻ��ġ�\n");  
+        printf("字符串是回文。\n");  
     } else {  
-        printf("�ַ������ǻ��ġ�\n");  
+        printf("字符串不是回文。\n");  
     }  
   
     return 0;  

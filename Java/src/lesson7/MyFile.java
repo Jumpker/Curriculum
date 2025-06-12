cb0ad83f332b7a36ca8db063b20666bc1c924a7d
Jker <2933385376@qq.com>
2025-06-12 09:09:57 +0800

    feat(调试配置): 添加MyFile调试配置并修改文件检查逻辑
    添加了VS Code的launch.json配置用于调试MyFile类
    修改MyFile.java以检查文件夹而非单个文件，并列出文件夹内容

--- Java/src/lesson7/MyFile.java
+++ Java/src/lesson7/MyFile.java
@@ -3,12 +3,16 @@ import java.io.File;
 
 public class MyFile {
     public static void main(String[] args) {
-        java.io.File file = new java.io.File("D:\\Jumpker\\MyPrograms\\Curriculum\\Java\\src\\lesson7\\MyFile.java");
+        java.io.File file = new java.io.File("D:\\Jumpker\\MyPrograms\\Curriculum\\Java\\src");
 
-        if (file.exists()) {
-            System.out.println("文件存在: " + file.getAbsolutePath());
+        if (file.exists() && file.isDirectory()) {
+            System.out.println("文件夹存在: " + file.getAbsolutePath());
+            String[] fs = file.list();
+            for (String f : fs) {
+                System.out.println(f);
+            }
         } else {
-            System.out.println("文件不存在: " + file.getAbsolutePath());
+            System.out.println("文件不存在或不是文件夹: " + file.getAbsolutePath());
         }
 
     }

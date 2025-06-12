d544ab977a5dac3628943de206b6187948706429
Jker <2933385376@qq.com>
2025-06-12 09:59:47 +0800

    feat: 添加递归搜索文件功能
    新增 SearchFile 方法用于递归搜索指定扩展名的文件，替换原有简单的目录列表功能。该方法能够遍历子目录并匹配文件名，提供更灵活的文件搜索能力。

--- Java/src/lesson7/MyFile.java
+++ Java/src/lesson7/MyFile.java
@@ -4,16 +4,24 @@ import java.io.File;
 public class MyFile {
     public static void main(String[] args) {
         java.io.File file = new java.io.File("D:\\Jumpker\\MyPrograms\\Curriculum\\Java\\src");
+        SearchFile(file, ".java");
+    }
 
-        if (file.exists() && file.isDirectory()) {
-            System.out.println("文件夹存在: " + file.getAbsolutePath());
-            String[] fs = file.list();
-            for (String f : fs) {
-                System.out.println(f);
-            }
-        } else {
-            System.out.println("文件不存在或不是文件夹: " + file.getAbsolutePath());
+    public static void SearchFile(File file, String name) {
+        if(file == null || !file.exists()){
+            System.out.println("文件不存在");
+            return;
         }
-
+        File[] f_temp = file.listFiles();
+        if(f_temp != null && f_temp.length > 0){
+            for(File f : f_temp){
+                if(f.isFile()){
+                    if(f.getName().contains(name))
+                        System.out.println(f.getAbsolutePath());
+                }else if(f.isDirectory()){
+                    SearchFile(f, name);
+                }
+            }
+        }
     }
 }
